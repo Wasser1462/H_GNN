7d476563716d2e7fa3d90f9742c66f7b02a28a9e
zengyongwang <wasserss@qq.com>
2024-12-06 17:37:40 +0800

    add data

---
+++ utils/check_model.py
@@ -0,0 +1,19 @@
+# Author: zyw
+# Date: 2024-11-01
+# Description: 
+import torch
+
+def print_model_parameters(model_path):
+    model = torch.load(model_path, map_location=torch.device('cpu'))
+    
+    if isinstance(model, dict) and 'state_dict' in model:
+        model = model['state_dict']
+    elif hasattr(model, 'state_dict'):
+        model = model.state_dict()
+    print("Model Parameters:\n")
+    for name, param in model.items():
+        print(f"Layer: {name} | Size: {param.size()} | Requires Grad: {param.requires_grad}")
+        print(f"Parameters:\n{param}\n")
+    
+model_path = '/data1/zengyongwang/test/H_GNN/result/model.pt'  
+print_model_parameters(model_path)
